ce5aab30e82107f065f22b1cce170b6da9101477
thomasn2712 <112751375+thomasn2712@users.noreply.github.com>
2023-04-19 17:54:15 -0500

    Added Plant Guide Menu
    Added Plant Guide Menu to tablet
    - displays information about growth times on plants
    - added day counter on main menu of tablet

--- Assets/PlantController.cs
+++ Assets/PlantController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlantController : MonoBehaviour
 {
-    public int day = 0;
+    public int day = 1;
+    public TMP_Text dayText;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class PlantController : MonoBehaviour
     public void incrementDay()
     {
         day += 1;
+        dayText.text = $"Day {day}";
         Debug.Log(day);
     }
 }
