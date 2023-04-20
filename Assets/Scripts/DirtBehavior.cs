77076dc53589c2ee073e5d1b62e53fcc68c01b4e
ColinBumpass <57447291+ReallyNotColin@users.noreply.github.com>
2023-04-20 05:51:41 -0500

    Planting Deletion WIP

--- Assets/Scripts/DirtBehavior.cs
+++ Assets/Scripts/DirtBehavior.cs
@@ -6,100 +6,163 @@ using UnityEngine;
 
 public class DirtBehavior : MonoBehaviour
 {
-    public GameObject dirtObject;
+    public PlantController currentday;
     public SeedlingBehavior seedlingBehavior;
-    public GameObject seedling;
 
+    public GameObject dirtObject;
+    public GameObject carrot;
+    public GameObject cabbage;
+    //public GameObject potato;
+    public GameObject carrotSeedling;
+    public GameObject cabbageSeedling;
+    //public GameObject potatoSeedling;
+    public GameObject carrotJuvie;
 
     public GameObject plant; // Change this to be a seedling (stages: seedling, growing, full grown)
-    public Material altmaterial;
     
+    public Material altmaterial;
     public MeshRenderer DirtRenderer1;
     public MeshRenderer DirtRenderer2;
     public MeshRenderer DirtRenderer3;
-   // public Collider watercollider;
+
     public bool TILLED;
     public bool PLANTED;
-    public bool FILLED;
-    public int filledCounter;
-
-    public PlantController currentday;
+    public bool FILLED; 
+    public int dirtFillCounter;
+    public int waterFillCounter;
+    private int plantlimit;
+    private int plantJuvilimit;
+    private string seedname;
     public int dayPlanted;
-    public bool REPLACE;
-
+    public bool WATERED;
     // Start is called before the first frame update
     void Start()
     {
-        seedlingBehavior = seedling.GetComponent<SeedlingBehavior>();
-        REPLACE = false;
+        plantlimit = 1;
+        plantJuvilimit = 1;
+        //seedlingBehavior = seedling.GetComponent<SeedlingBehavior>();
         TILLED = false;
         PLANTED = false;
         FILLED = false;
+        WATERED = false;
         dirtObject.GetComponent<MeshRenderer>().enabled = false;
         DirtRenderer1.enabled = false;
         DirtRenderer2.enabled = false;
         DirtRenderer3.enabled = false;
-        filledCounter = 0;
+        dirtFillCounter = 0;
+        waterFillCounter = 0;
         dayPlanted = -1;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // PLANT SEED 
-        if (other.gameObject.tag == "Seed" && TILLED && !PLANTED)
+        // FILL WITH DIRT 
+        if (other.gameObject.tag == "DirtDroplet")
         {
+            if (dirtFillCounter < 3)
+            {
+                Destroy(other.gameObject);
+                dirtFillCounter++;
+                print(dirtFillCounter);
+            }
+
+            if (dirtFillCounter == 3)
+            {
+                print("FILLED");
+                FILLED = true;
+            }
+        }
+        // PLANT SEED 
+        if (other.gameObject.name.EndsWith("Seed") && TILLED && !PLANTED)
+        {
+            seedname = other.gameObject.name;
             Destroy(other.gameObject);
             // Spawn seedling to be in the center of the dirt cube 
-            Instantiate(seedling, dirtObject.GetComponent<Renderer>().bounds.center, seedling.transform.rotation);
+            if (other.gameObject.name.StartsWith("Carrot"))
+            {
+                Instantiate(carrotSeedling, dirtObject.GetComponent<Renderer>().bounds.center, carrotSeedling.transform.rotation);
+
+            }
+            if (other.gameObject.name.StartsWith("Cabbage"))
+            {
+                Instantiate(cabbageSeedling, dirtObject.GetComponent<Renderer>().bounds.center, cabbageSeedling.transform.rotation);
+
+            }
+            /*
+            if (other.gameObject.name.StartsWith("Potato"))
+            {
+                Instantiate(seedling, dirtObject.GetComponent<Renderer>().bounds.center, seedling.transform.rotation);
+
+            }
+            */
             PLANTED = true;
             dayPlanted = currentday.day;
             // prints once no matter how many duplicates. 
             
         }
 
+        // WATER
         if (other.gameObject.tag == "Water" && TILLED && PLANTED)
         {
-            if (filledCounter < 3)
+            if (waterFillCounter < 3)
             {
                 Destroy(other.gameObject);
-                filledCounter++;
-                print(filledCounter);
+                waterFillCounter++;
+                print(waterFillCounter);
             }
 
-            if (filledCounter == 3)
+            if (waterFillCounter == 3)
             {
-                print("FILLED");
-                FILLED = true;
+                print("WATERED");
+                WATERED = true;
             }
         }
         
-        // FILL WITH DIRT 
-        if (other.gameObject.tag == "DirtDroplet")
-        {
-            if (filledCounter < 3)
-            {
-                Destroy(other.gameObject);
-                filledCounter++;
-                print(filledCounter);
-            }
-            
-            if (filledCounter == 3)
-            {
-                print("FILLED");
-                FILLED = true;
-            }
-        }
+        
         
     }
-    // TILL DIRT 
+
+    public void Planting()
+    {
+        
+        if (currentday.day == (dayPlanted + 1) && PLANTED && WATERED)
+        {
+            if (seedname == "CabbageSeed" && plantlimit > 0)
+            {
+                GameObject cabbageInstance;
+                cabbageInstance = Instantiate(cabbage, cabbageSeedling.GetComponent<Transform>().position, cabbageSeedling.GetComponent<Transform>().rotation) as GameObject;
+                Destroy(cabbageSeedling);
+                plantlimit--;
+            }
+
+            if (seedname == "CarrotSeed" && plantJuvilimit >0)
+            {
+                GameObject carrotInstance;
+                carrotInstance = Instantiate(carrotJuvie, carrotSeedling.GetComponent<Transform>().position, carrotSeedling.GetComponent<Transform>().rotation) as GameObject;
+                Destroy(carrotSeedling);
+                plantJuvilimit--;
+            }
+
+        }
+        if (currentday.day == (dayPlanted + 2) && PLANTED && WATERED)
+        {
+            if(seedname == "CarrotSeed" && plantlimit >0)
+            {
+                GameObject carrotInstance;
+                carrotInstance = Instantiate(carrot, carrotJuvie.GetComponent<Transform>().position, carrotJuvie.GetComponent<Transform>().rotation) as GameObject;
+                Destroy(carrotSeedling);
+                plantlimit--;
+            }
+        }
+    }
+     
     private void OnTriggerExit(Collider other)
     {
+        // TILL DIRT
         // Becomes Tilled upon Collision with Hoe Head leaving the cube's collider
         if (other.gameObject.tag == "Hoe" && FILLED)
         {
-            
             TILLED = true;
-            
         }
         if (other.gameObject.tag == "Shovel" && FILLED)
         {
@@ -114,14 +177,8 @@ public class DirtBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        /*
-        if (seedling.GetComponent<SeedlingBehavior>() != null)
-        {
-            print("ABLE TO FIND IT ");
-        }
-        */
-        //print(seedling.GetComponent<SeedlingBehavior>().WATERED);        
+        Planting();
+             
         if (FILLED)
         {
             DirtRenderer1.enabled = true;
@@ -138,35 +195,9 @@ public class DirtBehavior : MonoBehaviour
             DirtRenderer3.enabled = true;
         }
 
-        print("current value: " + currentday.day);
-        print("DayPlanted + 1: " + (dayPlanted + 1));
-        // Add more tags for more seeds
-        // Then make it so if tag was this do this 
-        // More booleans? 
-        if (currentday.day == (dayPlanted + 1) && PLANTED)
-        {
-
-            seedlingBehavior.Test();
-            REPLACE = true;
-            print("MADE IT ");
-            
-        }
-        /*
-         if (currentday.day > (dayPlanted + 1) && PLANTED)
-         {
-             REPLACE = true;
-             print("overshot ");
-             print("current value: " + currentday.day);
-             print("DayPlanted + 1: " + (dayPlanted + 1));
-         }
-         if (currentday.day < (dayPlanted + 1) && PLANTED)
-         {
-             REPLACE = true;
-             print("undershot ");
-             print("current value: " + currentday.day);
-             print("DayPlanted + 1: " + (dayPlanted + 1));
-         }
-            */
+        //print("current value: " + currentday.day);
+        //print("DayPlanted + 1: " + (dayPlanted + 1));
+    
 
     }
 }
